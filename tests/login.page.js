e9e5dbcc95b52b20b83ff231001b76fac163bf0e
AlinaLazea <alina_lazea@ymail.com>
2019-09-06 16:04:10 +0300

    page obj ex

---
+++ tests/login.page.js
@@ -0,0 +1,21 @@
+// login.page.js
+import Page from "./pageobject/page.js"
+
+class LoginPage extends Page {
+    get username() { return $('//*[@type="text" and @name="username"]'); }
+    get password() { return $('//*[@type="password" and @name="password"]'); }
+    get submitBtn() { return $('//*[@class ="radius" and @type="submit"]'); }
+    get flash() { return $('#flash'); }
+    get headerLinks() { return $$('#header a'); }
+
+    open() {
+        super.open('http://the-internet.herokuapp.com/login');
+    }
+
+    submit() {
+        this.submitBtn.click();
+    }
+
+}
+
+export default new LoginPage();

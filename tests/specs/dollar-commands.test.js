6bb21507d845a302468a8e6f32148186c3f3dbea
Alina Lazea <54836851+AlinaLazea@users.noreply.github.com>
2019-09-16 17:24:50 +0300

    fix circleCI (#13)
    * fix circleCI
    
    * config.yml run test fix
    
    * Added -browsers for docker image
    
    * Added -browsers for docker image without space

--- tests/specs/dollar-commands.test.js
+++
@@ -1,8 +0,0 @@
-
-describe("Fetch element(s)", () => {
-    it("should...", () => {
-        browser.url("https://the-internet.herokuapp.com/")
-        const links = $$("a")
-        links.forEach(link => console.log(link.getText()))
-    })
-})
